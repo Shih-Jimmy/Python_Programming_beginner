8f1741a5e6303d175aa7abc842f4a244e2d64510
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-08 19:23:29 +0800

    Create ex4-1.py

---
+++ ex4-1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print(type(name))
+print("Hello, " + name)
+
+age = int(input("Enter your age: "))
+print(type(age))
+
+height = float(input("Enter your height(m):  "))
+print(type(height))
