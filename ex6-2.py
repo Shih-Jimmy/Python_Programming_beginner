dc69b9b0e1d676a619c6abe13239903c100d77ff
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-22 19:34:18 +0800

    Create ex6-2.py

---
+++ ex6-2.py
@@ -0,0 +1,8 @@
+a = int(input("please input num a:"))
+b = int(input("please input num b:"))
+if a>b:
+    print("CTBC Brothers is the best!")
+    if a<1000000000:
+        print("Evolution!")
+    else:
+        print("Brothers!")
