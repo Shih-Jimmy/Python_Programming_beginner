248ea3b2ac766ef2997ce33f9fa1ff17f2cb2a91
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-22 20:52:02 +0800

    Create ex6-10.py

---
+++ ex6-10.py
@@ -0,0 +1,7 @@
+a = int(input("Please input a num a:"))
+import math
+print(a*a*math.pi)
+print(a*2*math.pi)
+print("---------------------------------------------------")
+print(a*a*3.14)
+print(a*2*3.14)
