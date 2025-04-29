cb63164e3c2dcfdd75d2e598c6bd5fb3c600a50f
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-29 19:47:41 +0800

    Create ex7-3.py

---
+++ ex7-3.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return x + y
+def mul(x,y):
+    return x*y
+x = int(input("Please input num x:"))
+y = int(input("Please input num y:"))
+print("x+y=",add(x,y))
+print("x*y=",mul(x,y))
