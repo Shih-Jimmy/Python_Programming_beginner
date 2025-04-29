64284d8aa09bdfe0329bbd4c83e94fd9d60adbdb
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-29 20:14:35 +0800

    Update ex7-3.py

--- ex7-3.py
+++ ex7-3.py
@@ -2,7 +2,13 @@ def add(x,y):
     return x + y
 def mul(x,y):
     return x*y
+def div(x,y):
+    return(x/y)
+def minus(x,y):
+    return(x-y)
 x = int(input("Please input num x:"))
 y = int(input("Please input num y:"))
 print("x+y=",add(x,y))
 print("x*y=",mul(x,y))
+print("x/y=",div(x,y))
+print("x-y=",minus(x,y))
