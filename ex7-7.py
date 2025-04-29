f1b0f026faa5286f4a909298d1902f64bc4b8b8d
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-29 20:54:17 +0800

    Create ex7-7.py

---
+++ ex7-7.py
@@ -0,0 +1,5 @@
+def Calculate_the_area_of_a_rectangle (bottom_base,height):
+    return bottom_base * height
+bottom_base = int(input("Please input bottom_base:"))
+height = int(input("Please input height:"))
+print("Calculate_the_area_of_a_rectangle is:",(bottom_base*height))
