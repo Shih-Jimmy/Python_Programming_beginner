868664645dc03c1795d985ed1e108433595e33bd
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-29 20:27:57 +0800

    Update ex7-5.py

--- ex7-5.py
+++ ex7-5.py
@@ -3,6 +3,16 @@ def factorial(n):
         return 1 
     else:
         print(n)
-    return factorial(n - 1)
+    return n* factorial(n - 1)
+    
+print(factorial(50))
+'''
+def factorial(n):
+    if n == 1:
+        #print(n)
+        return 1 
+    #else:
+        #print(n)
+    return n* factorial(n - 1)
     
 print(factorial(50))
