97eff7a96e575a0180bdea9bd145da24eb15934a
Shih-Jimmy <jimmy108075@gmail.com>
2025-04-29 21:01:35 +0800

    Create ex7-8.py

---
+++ ex7-8.py
@@ -0,0 +1,5 @@
+def Hours_and_minutes_conversion (hours,minutes):
+    return hours*60+minutes
+hours = int(input("Please input hours:"))
+minutes = int(input("Please input minutes:"))
+print("Hours_and_minutes_conversion:",(hours*60+minutes))
