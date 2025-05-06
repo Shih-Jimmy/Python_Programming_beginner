c43c2a53dad97a56877fb08fd190f7f09cbed933
Shih-Jimmy <jimmy108075@gmail.com>
2025-05-06 20:32:08 +0800

    Update ex8-3.py

--- ex8-3.py
+++ ex8-3.py
@@ -4,3 +4,15 @@ print(CPBL_Baseball_team)
     #print(CPBL_Baseball_team[i])
 for i in CPBL_Baseball_team:
     print(i)
+'''
+CPBL_Baseball_team = ["CTBC Brothers","Uni Lions","Rakutan","Fubon Guardians","TSG Hawk","Dragons"]
+print(CPBL_Baseball_team)
+#for i in range(0,3):
+    #print(CPBL_Baseball_team[i])
+for i in CPBL_Baseball_team:
+    print(i)
+
+CPBL_Baseball_team.append("Chinese Taipei")
+print(CPBL_Baseball_team)
+CPBL_Baseball_team.pop(5)
+print(CPBL_Baseball_team)
