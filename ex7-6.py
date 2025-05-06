56a20a8fdf20fa92908675fbf4db6290d7ec0223
Shih-Jimmy <jimmy108075@gmail.com>
2025-05-06 19:45:08 +0800

    Update ex7-6.py

--- ex7-6.py
+++ ex7-6.py
@@ -2,3 +2,15 @@ def minus (x):
     return x - 1911
 x = int(input("Please input AD:"))
 print("AD turn to Year of the Republic of China is:",minus(x))
+'''
+def my_func1(year):
+    return 1911 + year
+
+def my_func2(year):
+    return year - 1911
+
+year = int(input("Please input 民國 ="))
+print("西元 =",my_func1(year))
+
+year = int(input("Please input 西元 ="))
+print("民國民國 =",my_func2(year))
