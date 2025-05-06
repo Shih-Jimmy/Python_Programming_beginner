4dda9235cc12d78c470f625066c14a097fce0cf6
Shih-Jimmy <jimmy108075@gmail.com>
2025-05-06 20:14:29 +0800

    Create ex8-3.py

---
+++ ex8-3.py
@@ -0,0 +1,4 @@
+CPBL_Baseball_team = ["CTBC Brothers","Uni Lions","Rakutan","Fubon Guardians","TSG Hawk","Dragons"]
+print(CPBL_Baseball_team)
+for i in range(0,3):
+    print(CPBL_Baseball_team[i])
